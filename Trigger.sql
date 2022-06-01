b875b20144c52fa4333dcc7cf64ad1fe5dcb22c7
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-06-01 20:04:48 +0530

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -2,8 +2,13 @@ delimiter $$
 create trigger Trg1
 before update on employee
 for each row
+declare
+employee_id employee.eid%type;
 begin
 if new.salary>100000 then
 set new.salary=100000;
 end if;
 end$$
+/
+
+show error;
